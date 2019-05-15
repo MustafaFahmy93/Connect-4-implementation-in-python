a6da4c13827b8759fb113e31456fc20c15b95797
AliaaMahmoud <34009995+AliaaMahmoud@users.noreply.github.com>
2019-05-15 21:14:09 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,15 +2,17 @@
 import cv2 as cv
 import numpy as np
 import math
-import random
-
+from backend_v5 import *
+import time
+import threading
+thread = []
 #GLOBAL VARIABLES
 ix,iy = -1,-1
 cenX,cenY = 0,0
 turn = "us"
-board = np.zeros((6,7),np.uint8)
+board = np.zeros((6,7))
 img = np.zeros((710,720,3), np.uint8)
-
+win=False
 #FUNCTIONS
 def createBoard():
     global img
@@ -40,50 +42,65 @@ def getCenters(img): #I DIDN'T USE IT ANYWAYS
 def calculateColumn(x):
     col=math.ceil((ix)/100)
     return col
-        
+end=False      
 def guiEvents(event,x,y,flags,param): 
     global turn
     global ix,iy
     global board
     global cenX, cenY
-    global img
-    if event == cv.EVENT_LBUTTONDOWN:
+    global img,win
+    global end
+    if event == cv.EVENT_LBUTTONDOWN and end == False:
         ix,iy = x,y
         if(turn == "us"): #Play and switch turn
             col = calculateColumn(x)
-            if(board[0,col-1]== 1 or board[0,col-1]== 2): #COLUMN IS FULL, FORBIDDEN MOVE
+            if(board[5,col-1]== 1 or board[5,col-1]== 2): #COLUMN IS FULL, FORBIDDEN MOVE
                 return
             center(col)
+            if(check_winner(board,1)):
+                print("player 1 is winner")
+                end=True
+                win=True
             turn = "them"
             cv.circle(img,(cenX,cenY), 40, (0,0,255), -1)
-        elif(turn == "them"):
-            col = random.randint(1,6)
-            if(board[0,col-1]== 1 or board[0,col-1]== 2): #COLUMN IS FULL, FORBIDDEN MOVE
-                return
-            center(col)
-            turn= "us"
-            cv.circle(img,(cenX,cenY), 40, (0,255,0), -1)
-        
+def ai_turn():
+    global turn,img
+    global board,win,end
+    global cenX, cenY
+    while(True):
+        if(turn == "them" and end == False):
+                col=minimax(5, board, True, -math.inf, math.inf)[0]
+                col=col+1
+                print(board)
+                if(board[4,col-1]== 1 or board[5,col-1]== 2): #COLUMN IS FULL, FORBIDDEN MOVE
+                    return
+                center(col)
+                if(check_winner(board,2)):
+                    print("player 2 is winner")
+                    end=True
+                    win=True
+                turn= "us"
+                cv.circle(img,(cenX,cenY), 40, (0,255,0), -1)
 def center(col): #GET CENTER OF NEXT PLACE TO PLAY IN
     global turn
     global board
     global img
     global cenX, cenY
     for i in range(6):
-        if(board[5-i,col-1]==0 and turn == "us"):
-            board[5-i,col-1]=1
+        if(board[i,col-1]==0 and turn == "us"):
+            board[i,col-1]=1
             cenY=140+((5-i)*100)
             cenX=60+((col-1)*100)
             break
-        elif(board[5-i,col-1]==0 and turn == "them"):
-            board[5-i,col-1]=2
+        elif(board[i,col-1]==0 and turn == "them"):
+            board[i,col-1]=2
             cenY=140+((5-i)*100)
             cenX=60+((col-1)*100)
             break
-
+    
 #MAIN FUNCTION
 def main():
-    global img
+    global img,win,turn
     img = createBoard()
     cv.namedWindow('image')
     cv.setMouseCallback('image',guiEvents)
@@ -92,8 +109,11 @@ def main():
         k = cv.waitKey(20) & 0xFF
         if k == 27:
             break
+        elif k == 97:
+            turn="them"
+
     cv.destroyAllWindows()
-    
-    
-if __name__ =='__main__':
-    main()
+t2 = threading.Thread(target=main, args=[])
+t2.start()
+t1 = threading.Thread(target=ai_turn, args=[])
+t1.start()
