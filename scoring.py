819324898a723a2dc96564de19c3aef6e99bfe34
Mostafa Atef <m.atef96@hotmail.com>
2019-05-17 00:22:02 +0200

    Add files via upload

--- scoring.py
+++ scoring.py
@@ -1,7 +1,8 @@
-winner = []
+# winner = []
 gameScore = 100000
 def score_position(board,row, column, delta_y, delta_x):
-    global winner,gameScore
+    # global winner
+    global gameScore
     aiScore=0
     playerScore=0
     winningAI=[]
@@ -19,10 +20,10 @@ def score_position(board,row, column, delta_y, delta_x):
         column += delta_x
 
     if(playerScore == 4):
-        winner= winningPlayer
+        # winner= winningPlayer
         return -gameScore
     elif(aiScore == 4):
-        winner = winningAI
+        # winner = winningAI
         return gameScore
     else:
         return aiScore
@@ -52,7 +53,7 @@ def score(board):
                 return gameScore
             if(score == -gameScore):
                 return -gameScore
-            horizontalScores += gameScore
+            horizontalScores += gameScore#+100000
 
     for y in range (3):
         for x in range (4):
@@ -61,17 +62,235 @@ def score(board):
                 return gameScore
             if(score == -gameScore):
                 return -gameScore
-            diagonalScoresI += score
+            diagonalScoresI += score#+1
 
     for y in range(3,6):
-        for x in range (3):
+        for x in range (4):
             score = score_position(board,y, x, -1, 1) 
             if(score == gameScore):
                 return gameScore
             if(score == -gameScore):
                 return -gameScore
-            diagonalScoresII += score
+            diagonalScoresII += score#+1
 
     scores = horizontalScores + verticalScores + diagonalScoresI + diagonalScoresII
     return scores
 
+def scoreV2(board_tmp):
+    ai=2
+    player=1
+    aiCount=0
+    #==============0===============
+    for y in range(6):#right to left
+        for x in range(7):
+            if((x+3<7)and board_tmp[y,x]==0 and board_tmp[y,x+1]==0 and board_tmp[y,x+2]==0 and board_tmp[y,x+3]==0):
+                aiCount+=1
+    for x in range(7):#up to down
+        for y in range(6):
+            if((y+3<6)and board_tmp[y,x]==0 and board_tmp[y+1,x]==0 and board_tmp[y+2,x]==0 and board_tmp[y+3,x]==0):
+                aiCount+=1
+    for y in range(6):#diagonal  \
+        for x in range(7):
+            if((x+3<7) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x+1]==0 and board_tmp[y+2,x+2]==0 and board_tmp[y+3,x+3]==0):
+                aiCount+=1
+    for y in range(6):#diagonal  /
+        for x in range(7):
+            if((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==0 and board_tmp[y+2,x-2]==0 and board_tmp[y+3,x-3]==0):
+                aiCount+=1
+    #==============1===============
+    for y in range(6):#right to left
+        for x in range(7):
+            if((x+3<7)and board_tmp[y,x]==ai and board_tmp[y,x+1]==0 and board_tmp[y,x+2]==0 and board_tmp[y,x+3]==0):
+                aiCount+=2
+            elif((x+3<7)and board_tmp[y,x]==ai and board_tmp[y,x+1]==0 and board_tmp[y,x+2]==0 and board_tmp[y,x+3]==0):
+                aiCount-=2
+    for x in range(7):#up to down
+        for y in range(6):
+            if((y+3<6)and board_tmp[y,x]==ai and board_tmp[y+1,x]==0 and board_tmp[y+2,x]==0 and board_tmp[y+3,x]==0):
+                aiCount+=2
+            elif((y+3<6)and board_tmp[y,x]==ai and board_tmp[y+1,x]==0 and board_tmp[y+2,x]==0 and board_tmp[y+3,x]==0):
+                aiCount-=2
+    for y in range(6):#diagonal  \
+        for x in range(7):
+            if((x+3<7) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x+1]==0 and board_tmp[y+2,x+2]==0 and board_tmp[y+3,x+3]==0):
+                aiCount+=2
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x+1]==0 and board_tmp[y+2,x+2]==0 and board_tmp[y+3,x+3]==0):
+                aiCount-=2
+    for y in range(6):#diagonal  /
+        for x in range(7):
+            if((x-3>=0) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x-1]==0 and board_tmp[y+2,x-2]==0 and board_tmp[y+3,x-3]==0):
+                aiCount+=2
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x-1]==0 and board_tmp[y+2,x-2]==0 and board_tmp[y+3,x-3]==0):
+                aiCount-=2
+    #==============2===============
+    for y in range(6):#right to left
+        for x in range(7):
+            if((x+3<7)and board_tmp[y,x]==ai and board_tmp[y,x+1]==ai and board_tmp[y,x+2]==0 and board_tmp[y,x+3]==0):#(0-ai-ai-0)
+                aiCount+=3
+            elif((x+3<7)and board_tmp[y,x]==0 and board_tmp[y,x+1]==ai and board_tmp[y,x+2]==ai and board_tmp[y,x+3]==0):#(0-0-ai-ai) and (ai-0-0-ai)
+                aiCount+=3
+            elif((x+3<7)and board_tmp[y,x]==ai and board_tmp[y,x+1]==0 and board_tmp[y,x+2]==0 and board_tmp[y,x+3]==ai):#(ai-0-0-ai)
+                aiCount+=3
+            elif((x+3<7)and board_tmp[y,x]==0 and board_tmp[y,x+1]==0 and board_tmp[y,x+2]==ai and board_tmp[y,x+3]==ai):#(0-0-ai-ai)
+                aiCount+=3
+            elif((x+3<7)and board_tmp[y,x]==player and board_tmp[y,x+1]==player and board_tmp[y,x+2]==0 and board_tmp[y,x+3]==0):#(0-player-player-0)
+                aiCount-=5
+            elif((x+3<7)and board_tmp[y,x]==0 and board_tmp[y,x+1]==player and board_tmp[y,x+2]==player and board_tmp[y,x+3]==0):#(0-0-player-player) and (ai-0-0-ai)
+                aiCount-=5
+            elif((x+3<7)and board_tmp[y,x]==player and board_tmp[y,x+1]==0 and board_tmp[y,x+2]==0 and board_tmp[y,x+3]==player):#(player-0-0-player)
+                aiCount-=3
+            elif((x+3<7)and board_tmp[y,x]==0 and board_tmp[y,x+1]==0 and board_tmp[y,x+2]==player and board_tmp[y,x+3]==player):#(0-0-player-player)
+                aiCount-=5
+    for x in range(7):#up to down
+        for y in range(6):
+            if((y+3<6)and board_tmp[y,x]==ai and board_tmp[y+1,x]==ai and board_tmp[y+2,x]==0 and board_tmp[y+3,x]==0):#(ai-ai-0-0)
+                aiCount+=3
+            elif((y+3<6)and board_tmp[y,x]==0 and board_tmp[y+1,x]==ai and board_tmp[y+2,x]==ai and board_tmp[y+3,x]==0):#(0-ai-ai-0)
+                aiCount+=3
+            elif((y+3<6)and board_tmp[y,x]==0 and board_tmp[y+1,x]==0 and board_tmp[y+2,x]==ai and board_tmp[y+3,x]==ai):#(0-0-ai-ai)
+                aiCount+=3
+            elif((y+3<6)and board_tmp[y,x]==ai and board_tmp[y+1,x]==0 and board_tmp[y+2,x]==0 and board_tmp[y+3,x]==ai):#(ai-0-0-ai)
+                aiCount+=3
+            elif((y+3<6)and board_tmp[y,x]==player and board_tmp[y+1,x]==player and board_tmp[y+2,x]==0 and board_tmp[y+3,x]==0):#(player-player-0-0)
+                aiCount-=3
+            elif((y+3<6)and board_tmp[y,x]==0 and board_tmp[y+1,x]==player and board_tmp[y+2,x]==player and board_tmp[y+3,x]==0):#(0-player-player-0)
+                aiCount-=3
+            elif((y+3<6)and board_tmp[y,x]==0 and board_tmp[y+1,x]==0 and board_tmp[y+2,x]==player and board_tmp[y+3,x]==player):#(0-0-player-player)
+                aiCount-=3
+            elif((y+3<6)and board_tmp[y,x]==player and board_tmp[y+1,x]==0 and board_tmp[y+2,x]==0 and board_tmp[y+3,x]==player):#(player-0-0-player)
+                aiCount-=3
+    for y in range(6):#diagonal  \
+        for x in range(7):
+            if((x+3<7) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x+1]==ai and board_tmp[y+2,x+2]==0 and board_tmp[y+3,x+3]==0):#(ai-ai-0-0)
+                aiCount+=3
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x+1]==ai and board_tmp[y+2,x+2]==ai and board_tmp[y+3,x+3]==0):#(0-ai-ai-0)
+                aiCount+=3
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x+1]==0 and board_tmp[y+2,x+2]==ai and board_tmp[y+3,x+3]==ai):#(0-0-ai-ai)
+                aiCount+=3
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x+1]==0 and board_tmp[y+2,x+2]==0 and board_tmp[y+3,x+3]==ai):#(ai-0-0-ai)
+                aiCount+=3
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==player and board_tmp[y+1,x+1]==player and board_tmp[y+2,x+2]==0 and board_tmp[y+3,x+3]==0):#(player-player-0-0)
+                aiCount-=3
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x+1]==player and board_tmp[y+2,x+2]==player and board_tmp[y+3,x+3]==0):#(0-player-player-0)
+                aiCount-=3
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x+1]==0 and board_tmp[y+2,x+2]==player and board_tmp[y+3,x+3]==player):#(0-0-player-player)
+                aiCount-=3
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==player and board_tmp[y+1,x+1]==0 and board_tmp[y+2,x+2]==0 and board_tmp[y+3,x+3]==player):#(player-0-0-player)
+                aiCount-=3
+    for y in range(6):#diagonal  /
+        for x in range(7):
+            if((x-3>=0) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x-1]==ai and board_tmp[y+2,x-2]==0 and board_tmp[y+3,x-3]==0):#(ai-ai-0-0)
+                aiCount+=3
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==ai and board_tmp[y+2,x-2]==ai and board_tmp[y+3,x-3]==0):#(0-ai-ai-0)
+                aiCount+=3
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==0 and board_tmp[y+2,x-2]==ai and board_tmp[y+3,x-3]==ai):#(0-0-ai-ai)
+                aiCount+=3
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==ai and board_tmp[y+2,x-2]==ai and board_tmp[y+3,x-3]==0):#(0-ai-ai-0)
+                aiCount+=3
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==player and board_tmp[y+1,x-1]==player and board_tmp[y+2,x-2]==0 and board_tmp[y+3,x-3]==0):#(player-player-0-0)
+                aiCount-=3
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==player and board_tmp[y+2,x-2]==player and board_tmp[y+3,x-3]==0):#(0-player-player-0)
+                aiCount-=3
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==0 and board_tmp[y+2,x-2]==player and board_tmp[y+3,x-3]==player):#(0-0-player-player)
+                aiCount-=3
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==player and board_tmp[y+2,x-2]==player and board_tmp[y+3,x-3]==0):#(0-player-player-0)
+                aiCount-=3
+    #==============3===============
+    for y in range(6):#right to left
+        for x in range(7):
+            if((x+3<7)and board_tmp[y,x]==ai and board_tmp[y,x+1]==ai and board_tmp[y,x+2]==ai and board_tmp[y,x+3]==0):#(ai-ai-ai-0)
+                aiCount+=5
+            elif((x+3<7)and board_tmp[y,x]==0 and board_tmp[y,x+1]==ai and board_tmp[y,x+2]==ai and board_tmp[y,x+3]==ai):#(0-ai-ai-ai)
+                aiCount+=5
+            elif((x+3<7)and board_tmp[y,x]==ai and board_tmp[y,x+1]==0 and board_tmp[y,x+2]==ai and board_tmp[y,x+3]==ai):#(ai-0-ai-ai)
+                aiCount+=5
+            elif((x+3<7)and board_tmp[y,x]==0 and board_tmp[y,x+1]==ai and board_tmp[y,x+2]==ai and board_tmp[y,x+3]==ai):#(0-ai-ai-ai)
+                aiCount+=5
+            elif((x+3<7)and board_tmp[y,x]==player and board_tmp[y,x+1]==player and board_tmp[y,x+2]==player and board_tmp[y,x+3]==0):#(player-player-player-0)
+                aiCount-=7
+            elif((x+3<7)and board_tmp[y,x]==0 and board_tmp[y,x+1]==player and board_tmp[y,x+2]==player and board_tmp[y,x+3]==player):#(0-player-player-player)
+                aiCount-=7
+            elif((x+3<7)and board_tmp[y,x]==player and board_tmp[y,x+1]==0 and board_tmp[y,x+2]==player and board_tmp[y,x+3]==player):#(player-0-player-player)
+                aiCount-=7
+            elif((x+3<7)and board_tmp[y,x]==0 and board_tmp[y,x+1]==player and board_tmp[y,x+2]==player and board_tmp[y,x+3]==player):#(0-player-player-player)
+                aiCount-=7
+    for x in range(7):#up to down
+        for y in range(6):
+            if((y+3<6)and board_tmp[y,x]==ai and board_tmp[y+1,x]==ai and board_tmp[y+2,x]==ai and board_tmp[y+3,x]==0):#(ai-ai-ai-0)
+                aiCount+=5
+            elif((y+3<6)and board_tmp[y,x]==0 and board_tmp[y+1,x]==ai and board_tmp[y+2,x]==ai and board_tmp[y+3,x]==ai):#(0-ai-ai-ai)
+                aiCount+=5
+            elif((y+3<6)and board_tmp[y,x]==ai and board_tmp[y+1,x]==0 and board_tmp[y+2,x]==ai and board_tmp[y+3,x]==ai):#(ai-0-ai-ai)
+                aiCount+=5
+            elif((y+3<6)and board_tmp[y,x]==ai and board_tmp[y+1,x]==ai and board_tmp[y+2,x]==0 and board_tmp[y+3,x]==ai):#(ai-ai-0-ai)
+                aiCount+=5
+            elif((y+3<6)and board_tmp[y,x]==player and board_tmp[y+1,x]==player and board_tmp[y+2,x]==player and board_tmp[y+3,x]==0):#(player-player-player-0)
+                aiCount-=5
+            elif((y+3<6)and board_tmp[y,x]==0 and board_tmp[y+1,x]==player and board_tmp[y+2,x]==player and board_tmp[y+3,x]==player):#(0-player-player-player)
+                aiCount-=5
+            elif((y+3<6)and board_tmp[y,x]==player and board_tmp[y+1,x]==0 and board_tmp[y+2,x]==player and board_tmp[y+3,x]==player):#(player-0-player-player)
+                aiCount-=5
+            elif((y+3<6)and board_tmp[y,x]==player and board_tmp[y+1,x]==player and board_tmp[y+2,x]==0 and board_tmp[y+3,x]==player):#(player-player-0-player)
+                aiCount-=5
+    for y in range(6):#diagonal  \
+        for x in range(7):
+            if((x+3<7) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x+1]==ai and board_tmp[y+2,x+2]==ai and board_tmp[y+3,x+3]==0):#(ai-ai-ai-0)
+                aiCount+=7
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x+1]==ai and board_tmp[y+2,x+2]==ai and board_tmp[y+3,x+3]==ai):#(0-ai-ai-ai)
+                aiCount+=7
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x+1]==0 and board_tmp[y+2,x+2]==ai and board_tmp[y+3,x+3]==ai):#(ai-0-ai-ai)
+                aiCount+=7
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x+1]==ai and board_tmp[y+2,x+2]==ai and board_tmp[y+3,x+3]==ai):#(0-ai-ai-ai)
+                aiCount+=7
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==player and board_tmp[y+1,x+1]==player and board_tmp[y+2,x+2]==player and board_tmp[y+3,x+3]==0):#(player-player-player-0)
+                aiCount-=5
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x+1]==player and board_tmp[y+2,x+2]==player and board_tmp[y+3,x+3]==player):#(0-player-player-player)
+                aiCount-=5
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==player and board_tmp[y+1,x+1]==0 and board_tmp[y+2,x+2]==player and board_tmp[y+3,x+3]==player):#(player-0-player-player)
+                aiCount-=5
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x+1]==player and board_tmp[y+2,x+2]==player and board_tmp[y+3,x+3]==player):#(0-player-player-player)
+                aiCount-=5
+    for y in range(6):#diagonal  /
+        for x in range(7):
+            if((x-3>=0) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x-1]==ai and board_tmp[y+2,x-2]==ai and board_tmp[y+3,x-3]==0):#(ai-ai-ai-0)
+                aiCount+=7
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==ai and board_tmp[y+2,x-2]==ai and board_tmp[y+3,x-3]==ai):#(0-ai-ai-ai)
+                aiCount+=7
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x-1]==0 and board_tmp[y+2,x-2]==ai and board_tmp[y+3,x-3]==ai):#(ai-0-ai-ai)
+                aiCount+=7
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==ai and board_tmp[y+2,x-2]==ai and board_tmp[y+3,x-3]==ai):#(0-ai-ai-ai)
+                aiCount+=7
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==player and board_tmp[y+1,x-1]==player and board_tmp[y+2,x-2]==player and board_tmp[y+3,x-3]==0):#(player-player-player-0)
+                aiCount-=5
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==player and board_tmp[y+2,x-2]==player and board_tmp[y+3,x-3]==player):#(0-player-player-player)
+                aiCount-=5
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==player and board_tmp[y+1,x-1]==0 and board_tmp[y+2,x-2]==player and board_tmp[y+3,x-3]==player):#(player-0-player-player)
+                aiCount-=5
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==0 and board_tmp[y+1,x-1]==player and board_tmp[y+2,x-2]==player and board_tmp[y+3,x-3]==player):#(0-player-player-player)
+                aiCount-=5
+    #==============4===============
+    for y in range(6):#right to left
+        for x in range(7):
+            if((x+3<7) and board_tmp[y,x]==ai and board_tmp[y,x+1]==ai and board_tmp[y,x+2]==ai and board_tmp[y,x+3]==ai ):
+                aiCount+=100
+            elif((x+3<7) and board_tmp[y,x]==player and board_tmp[y,x+1]==player and board_tmp[y,x+2]==player and board_tmp[y,x+3]==player ):
+                aiCount-=50
+    for x in range(7):#up to down
+        for y in range(6):
+            if((y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x]==ai and board_tmp[y+2,x]==ai and board_tmp[y+3,x]==ai):
+                aiCount+=100
+            elif((y+3<6) and board_tmp[y,x]==player and board_tmp[y+1,x]==player and board_tmp[y+2,x]==player and board_tmp[y+3,x]==player):
+                aiCount-=50
+    for y in range(6):#diagonal  \
+        for x in range(7):
+            if((x+3<7) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x+1]==ai and board_tmp[y+2,x+2]==ai and board_tmp[y+3,x+3]==ai):
+                aiCount+=100
+            elif((x+3<7) and (y+3<6) and board_tmp[y,x]==player and board_tmp[y+1,x+1]==player and board_tmp[y+2,x+2]==player and board_tmp[y+3,x+3]==player):
+                aiCount-=50
+    for y in range(6):#diagonal  /
+        for x in range(7):
+            if((x-3>=0) and (y+3<6) and board_tmp[y,x]==ai and board_tmp[y+1,x-1]==ai and board_tmp[y+2,x-2]==ai and board_tmp[y+3,x-3]==ai):
+                aiCount+=100
+            elif((x-3>=0) and (y+3<6) and board_tmp[y,x]==player and board_tmp[y+1,x-1]==player and board_tmp[y+2,x-2]==player and board_tmp[y+3,x-3]==player):
+                aiCount-=50
+    return aiCount
